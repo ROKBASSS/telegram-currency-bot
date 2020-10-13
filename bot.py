acf7f84e547a893a6202ef5bbc938e3f9d2e66e0
ROKBASSS <anton.fomi@ya.ru>
2020-10-14 01:37:54 +0300

    bug fixes #2

--- bot.py
+++ bot.py
@@ -20,11 +20,15 @@ bot = telebot.TeleBot(token, num_threads=6)
 task = task()
 telebot.logger.setLevel(logging.INFO)
 
+
 @bot.message_handler(commands=['start', 'go', 'help'])
 def handle_start(message):
-    bot.reply_to(message, "Привет партнёр! Сейчас доступны: Сбербанк, ВТБ, Тинькофф")
-    msg = bot.send_message(message.chat.id, 'Какой банк вы хотите?', reply_markup=m.start_markup)
-    logging.info('Пользователь {} начал общение с ботом!'.format(message.chat.id))
+    bot.reply_to(
+        message, "Привет партнёр! Сейчас доступны: Сбербанк, ВТБ, Тинькофф")
+    msg = bot.send_message(
+        message.chat.id, 'Какой банк вы хотите?', reply_markup=m.start_markup)
+    logging.info(
+        'Пользователь {} начал общение с ботом!'.format(message.chat.id))
     print('Пользователь {} начал общение с ботом!'.format(message.chat.id))
     bot.register_next_step_handler(msg, askCurrencies)
     return
@@ -42,19 +46,23 @@ def askCurrencies(message):
     chat_id = message.chat.id
     text = message.text.lower()
     if(text == 'сбербанк'):
-        msg = bot.send_message(chat_id, 'Какую валюту вы хотите?', reply_markup=m.source_markup)
+        msg = bot.send_message(
+            chat_id, 'Какую валюту вы хотите?', reply_markup=m.source_markup)
         bot.register_next_step_handler(msg, getValuesSber)
         return
     elif(text == 'втб'):
-        msg = bot.send_message(chat_id, 'Какую валюту вы хотите?', reply_markup=m.source_markup)
+        msg = bot.send_message(
+            chat_id, 'Какую валюту вы хотите?', reply_markup=m.source_markup)
         bot.register_next_step_handler(msg, getValuesVtb)
         return
     elif(text == 'тинькофф'):
-        msg = bot.send_message(chat_id, 'Какую валюту вы хотите?', reply_markup=m.source_markup)
+        msg = bot.send_message(
+            chat_id, 'Какую валюту вы хотите?', reply_markup=m.source_markup)
         bot.register_next_step_handler(msg, getValuesTink)
         return
     else:
-        msg = bot.send_message(chat_id, 'Нет такого банка. Какой банк вы хотите?',reply_markup=m.start_markup)
+        msg = bot.send_message(
+            chat_id, 'Нет такого банка. Какой банк вы хотите?', reply_markup=m.start_markup)
         bot.register_next_step_handler(msg, askCurrencies)
         return
 
@@ -83,7 +91,7 @@ def getValuesSber(message):
         return
     else:
         msg = bot.send_message(
-            chat_id, 'Такой валюты нет. Выберите корректную валюту')
+            chat_id, 'Такой валюты нет. Выберите корректную валюту', reply_markup=m.source_markup)
         bot.register_next_step_handler(msg, getValuesSber)
         task.isRunning = False
         return
@@ -113,7 +121,7 @@ def getValuesVtb(message):
         return
     else:
         msg = bot.send_message(
-            chat_id, 'Такой валюты нет. Выберите корректную валюту')
+            chat_id, 'Такой валюты нет. Выберите корректную валюту', reply_markup=m.source_markup)
         bot.register_next_step_handler(msg, getValuesVtb)
         task.isRunning = False
         return
@@ -124,23 +132,26 @@ def getValuesTink(message):
     text = message.text.lower()
     if text in task.names[0]:
         value = tinkoff.getValues("dollar")
-        msg = bot.send_message(chat_id, 'Покупается $ по {}, продаётся $ по {}'.format(value[0], value[1]), reply_markup=m.source_markup)
+        msg = bot.send_message(chat_id, 'Покупается $ по {}, продаётся $ по {}'.format(
+            value[0], value[1]), reply_markup=m.source_markup)
         bot.register_next_step_handler(msg, getValuesTink)
         task.isRunning = False
         return
     elif text in task.names[1]:
         value = tinkoff.getValues("euro")
-        msg = bot.send_message(chat_id, 'Покупается € по {}, продаётся € по {}'.format(value[0], value[1]), reply_markup=m.source_markup)
+        msg = bot.send_message(chat_id, 'Покупается € по {}, продаётся € по {}'.format(
+            value[0], value[1]), reply_markup=m.source_markup)
         bot.register_next_step_handler(msg, getValuesTink)
         task.isRunning = False
         return
     elif text == "назад":
-        msg = bot.send_message(chat_id, 'Какой банк вы хотите?',reply_markup=m.start_markup)
+        msg = bot.send_message(
+            chat_id, 'Какой банк вы хотите?', reply_markup=m.start_markup)
         bot.register_next_step_handler(msg, askCurrencies)
         return
     else:
         msg = bot.send_message(
-            chat_id, 'Такой валюты нет. Выберите корректную валюту')
+            chat_id, 'Такой валюты нет. Выберите корректную валюту', reply_markup=m.source_markup)
         bot.register_next_step_handler(msg, getValuesTink)
         task.isRunning = False
         return
